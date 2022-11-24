122a8382fd82ec92ab0df53847ed9f8e605c7407
Wang Chen <wangchen20@iscas.ac.cn>
2022-11-24 09:29:54 +0800

    added 5th issue for aosp review

--- articles/android-review/build.sh
+++ articles/android-review/build.sh
@@ -7,6 +7,7 @@ ISSUE_STRING="\
 2022-10-13 2022-10-28;\
 2022-10-27 2022-11-11;\
 2022-11-10 2022-11-25;\
+2022-11-24 2022-12-09;\
 "
 
 IFS=';' read -ra ISSUE_ARRAY <<< "$ISSUE_STRING"
@@ -17,4 +18,5 @@ for ISSUE in "${ISSUE_ARRAY[@]}"; do
 	sh ./build_html.sh ${ARGS[0]} ${ARGS[1]}
 done
 
+mv aosp-riscv-*.html ../../../unicornx.github.io/android-review/
 
