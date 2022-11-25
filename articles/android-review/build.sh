ed37403f83ba30358dbf8c3d7abcbdf2b0dcf042
Wang Chen <wangchen20@iscas.ac.cn>
2022-11-25 13:38:01 +0800

    unified the timestamp for aosp-review

--- articles/android-review/build.sh
+++ articles/android-review/build.sh
@@ -3,11 +3,11 @@
 
 # update this as per tracking period
 ISSUE_STRING="\
-2022-09-30 2022-10-14;\
-2022-10-13 2022-10-28;\
-2022-10-27 2022-11-11;\
-2022-11-10 2022-11-25;\
-2022-11-24 2022-12-09;\
+2022-09-30 ~ 2022-10-14;\
+2022-10-13 ~ 2022-10-28;\
+2022-10-27 ~ 2022-11-11;\
+2022-11-10 ~ 2022-11-25;\
+2022-11-24 ~ 2022-12-09;\
 "
 
 IFS=';' read -ra ISSUE_ARRAY <<< "$ISSUE_STRING"
@@ -15,7 +15,8 @@ IFS=';' read -ra ISSUE_ARRAY <<< "$ISSUE_STRING"
 for ISSUE in "${ISSUE_ARRAY[@]}"; do
 #	echo $ISSUE
 	ARGS=($ISSUE)
-	sh ./build_html.sh ${ARGS[0]} ${ARGS[1]}
+#	echo ${ARGS[0]} ${ARGS[1]} ${ARGS[2]}
+	sh ./build_html.sh ${ARGS[0]} ${ARGS[2]}
 done
 
 mv aosp-riscv-*.html ../../../unicornx.github.io/android-review/
