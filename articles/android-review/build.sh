197cb86107c3a68c0cd015d512d7d8aa00b74a94
Wang Chen <wangchen20@iscas.ac.cn>
2023-04-16 11:04:08 +0800

    added 15th aosp review
    Signed-off-by: Wang Chen <wangchen20@iscas.ac.cn>

--- articles/android-review/build.sh
+++ articles/android-review/build.sh
@@ -2,16 +2,16 @@
 # Currently only support running this under the same dir as this script
 
 # Update these vars as per tracking period
-ISSUE_NO="14"
+ISSUE_NO="15"
 # this Thursday ~ next next Friday
-ISSUE_STRING="2023-03-30 ~ 2023-04-14"
+ISSUE_STRING="2023-04-13 ~ 2023-04-28"
 ARGS=($ISSUE_STRING)
 #echo ${ARGS[0]} ${ARGS[1]} ${ARGS[2]}
 
 # Update files in working-group
 NEWFILE="./${ARGS[2]}.md"
 touch $NEWFILE
-echo -n "本周期（" >> $NEWFILE
+echo -n "第 ${ISSUE_NO} 期（" >> $NEWFILE
 echo -n $ISSUE_STRING >> $NEWFILE
 echo -e "）RISCV 相关 merge PR 汇总\n" >> $NEWFILE
 echo -e "可以通过这个链接过滤 ${ARGS[2]} 之前的 patch: <https://android-review.googlesource.com/q/mergedbefore:+${ARGS[2]}+AND+riscv64>。\n"  >> $NEWFILE
