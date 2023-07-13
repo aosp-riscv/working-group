46e94f3da22dadefb7c9cb6f64bd52e3dd16d7f8
Wang Chen <wangchen20@iscas.ac.cn>
2023-07-13 08:19:11 +0800

    aosp review, 21st issue
    Signed-off-by: Wang Chen <wangchen20@iscas.ac.cn>

--- articles/android-review/build.sh
+++ articles/android-review/build.sh
@@ -2,9 +2,9 @@
 # Currently only support running this under the same dir as this script
 
 # Update these vars as per tracking period
-ISSUE_NO="20"
+ISSUE_NO="21"
 # this Thursday ~ next next Friday
-ISSUE_STRING="2023-06-22 ~ 2023-07-07"
+ISSUE_STRING="2023-07-06 ~ 2023-07-21"
 ARGS=($ISSUE_STRING)
 #echo ${ARGS[0]} ${ARGS[1]} ${ARGS[2]}
 
